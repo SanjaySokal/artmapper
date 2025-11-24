d57cbcd01fe68465798f99f74b805c36d3302763
Sanjay Sokal Work <sanjaysokalwork@gmail.com>
2025-11-24 15:17:39 +0530

    Fix export syntax and add config files to package

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,103 @@
 // Core exports
-export * from './core/decorators/entity';
-export * from './core/decorators/mapping';
-export * from './core/decorators/component';
-export * from './core/decorators/lombok';
-export * from './core/decorators/web';
+export {
+  Entity,
+  Table,
+  Column,
+  Id,
+  GeneratedValue,
+  OneToMany,
+  ManyToOne,
+  ManyToMany,
+  OneToOne,
+  ENTITY_METADATA_KEY,
+  TABLE_METADATA_KEY,
+  COLUMN_METADATA_KEY,
+  ID_METADATA_KEY,
+  GENERATED_VALUE_METADATA_KEY,
+  type ColumnOptions,
+  type GeneratedValueOptions,
+  type RelationOptions
+} from './core/decorators/entity';
 
-export * from './core/orm/EntityManager';
-export * from './core/orm/Repository';
-export * from './core/orm/RelationshipManager';
+export {
+  JoinColumn,
+  JoinColumns,
+  JoinTable,
+  MappedSuperclass,
+  Embedded,
+  Embeddable,
+  Transient,
+  Enumerated,
+  Temporal,
+  type JoinColumnOptions,
+  type JoinTableOptions,
+  type EnumType,
+  type TemporalType
+} from './core/decorators/mapping';
 
-export * from './core/database/DatabaseConfig';
-export * from './core/database/SchemaGenerator';
+export {
+  Component,
+  Service,
+  Controller,
+  Repository,
+  Autowired,
+  Value,
+  Qualifier
+} from './core/decorators/component';
 
-export * from './core/di/Container';
+export {
+  Data,
+  Getter,
+  Setter,
+  Builder,
+  ToString,
+  EqualsAndHashCode,
+  NoArgsConstructor,
+  AllArgsConstructor,
+  RequiredArgsConstructor
+} from './core/decorators/lombok';
 
-export * from './core/application/SpringApplication';
+export {
+  GetMapping,
+  PostMapping,
+  PutMapping,
+  DeleteMapping,
+  PatchMapping,
+  RequestBody,
+  RequestParam,
+  PathVariable,
+  RequestHeader
+} from './core/decorators/web';
+
+export {
+  EntityManager,
+  QueryBuilder
+} from './core/orm/EntityManager';
+
+export {
+  BaseRepository,
+  CrudRepository
+} from './core/orm/Repository';
+
+export {
+  RelationshipManager
+} from './core/orm/RelationshipManager';
+
+export {
+  DatabaseManager,
+  type DatabaseConfig
+} from './core/database/DatabaseConfig';
+
+export {
+  SchemaGenerator
+} from './core/database/SchemaGenerator';
+
+export {
+  Container
+} from './core/di/Container';
+
+export {
+  SpringApplication,
+  type ApplicationConfig
+} from './core/application/SpringApplication';
 
